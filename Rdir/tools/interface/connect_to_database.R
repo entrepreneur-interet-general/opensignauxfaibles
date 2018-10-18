10eca958a4c711bb7b848d5ecc0601877e3dd9e7
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2018-10-18 09:46:30 +0200

    Improve db connection
    v2

--- Rdir/tools/interface/connect_to_database.R
+++ Rdir/tools/interface/connect_to_database.R
@@ -3,7 +3,9 @@ connect_to_database <- function(
   batch,
   algo = "algo2",
   siren = NULL,
-  min_effectif = 20){
+  min_effectif = 10,
+  fields = NULL){
+
   cat("Connexion à la collection mongodb ...")
   dbconnection <- mongo(
     collection = collection,
@@ -12,83 +14,87 @@ connect_to_database <- function(
     url = "mongodb://localhost:27017")
   cat(" Fini.", "\n")
 
-  cat("Import ...")
-  if (is.null(siren)){
-    requete  <- paste0('[{"$match":{"_id.batch":"', batch, '",
-      "_id.algo":"', algo, '"}},
-      {"$unwind":{"path": "$value"}}]')
-} else {
-  match_siren  <- '"$or":['
-  for (i in seq_along(siren)){
-    match_siren  <- paste0(
-      match_siren,
-      '{"_id.siren":"',
-      siren[i],
-      '"},'
-      )
+  # Construction du match
+  match_req  <- paste0('"_id.batch":"', batch, '","_id.algo":"', algo, '"')
+  if (!is.null(siren)){
+    match_siren  <- c()
+    for (i in seq_along(siren)){
+      match_siren  <- c(
+        match_siren,
+        paste0('{"_id.siren":"', siren[i], '"}')
+        )
+    }
+    match_siren <- paste0('"$or":[', paste(match_siren, collapse = ","), "]")
+    match_req <- paste(match_req, match_siren, sep = ", ")
   }
-  match_siren <- paste0(
-    substr(match_siren, 1, nchar(match_siren) - 1),
+  match_req <- paste0('{"$match":{', match_req, "}}")
+
+  # Construction de l'unwind
+  unwind_req <- '{"$unwind":{"path": "$value"}}'
+
+  # Filtrage effectif
+  if (!is.null(siren)){
+    eff_req <- ""
+  } else {
+    eff_req <- paste0(
+      '{"$match":{"value.effectif":{"$gte":',
+      min_effectif,
+      '}}}')
+  }
+
+  # Construction de la projection
+  if (is.null(fields)){
+    projection_req  <- ""
+  } else {
+    projection_req  <- paste0('"value.',fields,'":1')
+    projection_req  <- paste(projection_req, collapse = ",")
+    projection_req  <- paste0('{"$project":{', projection_req, '}}')
+  }
+
+  reqs <- c(
+    match_req,
+    unwind_req,
+    eff_req,
+    projection_req)
+
+  requete  <- paste(
+    reqs[reqs != ""],
+    collapse = ", ")
+  requete <- paste0(
+    "[",
+    requete,
     "]")
 
-  requete  <- paste0('[{"$match":{"_id.batch":"', batch, '",
-    "_id.algo":"', algo, '",',
-    match_siren, '}},
-    {"$unwind":{"path": "$value"}}]')
-}
 
-  data <- dbconnection$aggregate(requete)$value
+  cat("Import ...")
 
-  if (algo == "algo1") {
-    # FIX ME: rien à faire ici
-    cat("FIX ME ... feature engineering a effectuer ailleurs\n")
-    data <- data %>%
-      mutate(
-        cut_growthrate = forcats::fct_relevel(
-          cut_growthrate,
-          c("moins_de_20p", "moins_20_a_5p", "stable",
-            "plus_5_a_20p", "plus_20p", "manquant")),
-        cut_effectif = forcats::fct_relevel(cut_effectif),
-        outcome_0_12 = forcats::fct_relevel(outcome_0_12,
-          c("default", "non_default")
-          )
-        )
-  }
+  donnees <- dbconnection$aggregate(requete)$value
 
   cat(" Fini.", "\n")
-  assertthat::assert_that(nrow(data)>0, msg = "La requête retourne aucun résultat")
-  assertthat::assert_that(all(c("periode", "siret", "effectif", "code_ape") %in% names(data)))
-  assertthat::assert_that(anyDuplicated(data %>% select(siret, periode)) == 0)
 
-  table_wholesample <- data %>%
+  assertthat::assert_that(nrow(donnees) > 0,
+    msg = "La requête ne retourne aucun résultat")
+  assertthat::assert_that(
+    all(c("periode", "siret") %in% names(donnees))
+    )
+  assertthat::assert_that(
+    anyDuplicated(donnees %>% select(siret, periode)) == 0
+    )
+
+  table_wholesample <- donnees %>%
     mutate(periode = as.Date(periode)) %>%
     arrange(periode) %>%
     tibbletime::as_tbl_time(periode)
 
-  n_eta <- table_wholesample$siret %>% n_distinct()
-  n_ent <- table_wholesample$siret %>% str_sub(1, 9) %>% n_distinct()
+  n_eta <- table_wholesample$siret %>%
+    n_distinct()
+  n_ent <- table_wholesample$siret %>%
+    str_sub(1, 9) %>%
+    n_distinct()
   cat("Import de", n_eta, "etablissements issus de", n_ent, "entreprises", "\n")
 
-  if (is.null(siren)){
-    cat("Filtrage des établissements avec des effectifs de moins de", min_effectif, "...")
-    table_wholesample  <- table_wholesample %>%
-      group_by(siret) %>%
-      mutate(toKeep = max(effectif)> min_effectif) %>%
-      filter(toKeep) %>%
-      select(-toKeep)
-  }
 
-  if (algo == "algo1") {
-    table_wholesample <- table_wholesample %>%
-      dplyr::mutate(proc_collective = if_else(any(!is.na(date_defaillance)), max(date_defaillance, na.rm =TRUE), as.POSIXct(NA)))
-  }
-
-  table_wholesample <- table_wholesample %>%
-    ungroup()
   cat(" Fini.", "\n")
-  n_eta_filtered <- table_wholesample$siret %>% n_distinct()
-  n_ent_filtered <-  table_wholesample$siret %>% str_sub(1, 9) %>% n_distinct()
-  cat(n_eta_filtered, "etablissements restants, de", n_ent_filtered, "entreprises", "\n")
 
   cat("Import des libellé NAF niveaux 1 et 5 ...")
 
@@ -125,11 +131,10 @@ connect_to_database <- function(
     )
 
   table_wholesample <- table_wholesample %>%
-    left_join(libelle_naf, by = c("code_ape"= "code_naf_niveau5")) %>%
+    left_join(libelle_naf, by = c("code_ape" = "code_naf_niveau5")) %>%
     mutate(code_ape = as.factor(code_ape))
 
   cat(" Fini.", "\n")
 
   return(table_wholesample)
 }
-
