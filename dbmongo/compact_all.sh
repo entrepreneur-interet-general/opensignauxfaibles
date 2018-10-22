65a4e23172faed32a1dfa425c2baeadf28e8faf4
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2018-10-22 14:10:42 +0200

    Trivial bash scripts

---
+++ dbmongo/compact_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+http :3000/api/compact/etablissement
+http :3000/api/compact/entreprise
+http :3000/api/reduce/algo2/1810
---
+++ dbmongo/import_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+http :3000/api/purge
+http :3000/api/import/1802
+http :3000/api/import/1803
+http :3000/api/import/1804
+http :3000/api/import/1805
+http :3000/api/import/1806
+http :3000/api/import/1807
+http :3000/api/import/1808
+http :3000/api/import/1809
+http :3000/api/import/1810
