34ba88d2d66d92815cfc15d0afa291c0190e2eea
Pierre Camilleri <camilleri_pierre@hotmail.fr>
2018-12-13 16:27:46 +0100

    Reworking union to handle big objects

--- dbmongo/js/algo2/union/reduce.js
+++ dbmongo/js/algo2/union/reduce.js
@@ -1,7 +1,8 @@
 function reduce(key, values) {
-  if (values.length > 40){
-    return {}
-  }
+  //if (values.length > 40){
+  //  return {}
+  //
+  //}
   return values.reduce((m, v) => {
     Object.assign(m, v)
     return m
